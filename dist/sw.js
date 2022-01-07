6b5412a2b2374eec8bd6cb255c339a2f4c8b26bb
Ermogenes Palacio <epalacio@prodam.sp.gov.br>
2022-01-07 15:09:33 -0300

    Improve sw

--- dist/sw.js
+++ dist/sw.js
@@ -11,6 +11,8 @@ const cacheableAssets = [
   "manifest.json",
   "maskable_icon_x512.png",
   "style.css",
+  "https://avatars.githubusercontent.com/u/14313064",
+  "https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&display=swap",
 ];
 
 self.addEventListener("install", (event) => {
@@ -19,6 +21,18 @@ self.addEventListener("install", (event) => {
       .open("buy-me-a-beer-assets")
       .then((cache) => cache.addAll(cacheableAssets))
   );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      if ("navigationPreload" in self.registration) {
+        await self.registration.navigationPreload.enable();
+      }
+    })()
+  );
+
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
