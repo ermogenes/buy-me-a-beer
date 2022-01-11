9386a8697b9b33aa5435f21949a1a1e115326469
ermogenes <ermogenes@users.noreply.github.com>
2022-01-11 13:12:55 +0000

    Deploying to gh-pages from @ ermogenes/buy-me-a-beer@f7d1a8f1b5f97dc1221c17113acf92b12ad48c59 🚀

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+if (!window["getDigitalGoodsService"]) {
+  window.location.replace("https://play.google.com/store/apps/details?id=io.github.ermogenes.twa");
+} else {
+  try {
+    const digitalGoodsService = await window.getDigitalGoodsService("https://play.google.com/billing");
+    console.log('gplay digitalGoodsService available', digitalGoodsService);
+    if (service) {
+      console.log('api v2');
+    } else {
+      console.log('api v1');
+    }
+  } catch (error) {
+    console.log('api v2 and gplaydigitalGoodsService not available');
+  }
+}
+
 const beerPrice = 20;
 const coffeePrice = 10;
 
